3adc82484ba76284f7b4e17a9923f9687f9afe0b
Fernando Alegría <fernanalegria@gmail.com>
2019-04-12 19:17:27 +0200

    fix: Fix form width

--- app/views/common/Form.js
+++ app/views/common/Form.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-const Form = ({ children }) => <View style={styles.form}>{children}</View>;
+const Form = ({ children }) => (
+  <View style={styles.container}>
+    <View style={styles.form}>{children}</View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   form: {
-    paddingLeft: 50,
-    paddingRight: 50,
+    width: 280,
     justifyContent: 'center',
     alignItems: 'stretch'
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
