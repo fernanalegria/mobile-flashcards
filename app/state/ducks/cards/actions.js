0247037f4955ef7f6c5a8159a9df01329d159c9d
Fernando Alegría <fernanalegria@gmail.com>
2019-04-13 06:58:02 +0200

    docs: Add JSDoc comments to action creators and custom middleware

--- app/state/ducks/cards/actions.js
+++ app/state/ducks/cards/actions.js
@@ -1,23 +1,45 @@
 import { saveCard, fetchCards } from 'server/api';
 import * as types from './types';
 
+/**
+ * Action creator for the type ADD_CARD
+ * @param  {Object} card
+ * @param  {number} deckId
+ * @returns  {Object} Action
+ */
 const addCard = (card, deckId) => ({
   type: types.ADD_CARD,
   card,
   deckId
 });
 
+/**
+ * Makes a request to the API to save a new card and stores it in Redux
+ * @param  {Object} card
+ * @param  {number} deckId
+ * @returns  {Promise}
+ */
 export const handleAddCard = (card, deckId) => dispatch =>
   saveCard(card, deckId).then(result => {
     dispatch(addCard(result, deckId));
     return Promise.resolve(result);
   });
 
+/**
+ * Action creator for the type RECEIVE_CARDS
+ * @param  {Object} cards
+ * @returns  {Object} Action
+ */
 const receiveCards = cards => ({
   type: types.RECEIVE_CARDS,
   cards
 });
 
+/**
+ * Makes a request to fetch the existing cards
+ * from the AsyncStorage and stores them in Redux
+ * @returns  {Promise}
+ */
 export const handleReceiveCards = () => dispatch =>
   fetchCards().then(cards => {
     dispatch(receiveCards(cards));
